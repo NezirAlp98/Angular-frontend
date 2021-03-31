9e937256dc0aa2d16ef1cfb25b3a23a9a2591315
nezir alp arslan <nezirarslan1234@gmail.com>
2021-03-31 21:15:30 +0300

    31 mart commit 2

--- src/app/components/product/product.component.ts
+++ src/app/components/product/product.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Product } from 'src/app/models/product';
+import { HttpClient } from '@angular/common/http'
+import { ProductResponseModel } from 'src/app/models/productResponseModel';
 
 @Component({
   selector: 'app-product',
@@ -9,9 +11,19 @@ import { Product } from 'src/app/models/product';
 export class ProductComponent implements OnInit {
   
   products:Product[] = [];
-  constructor() { }
+  apiUrl="https://localhost:44304/api/products/getall";
+  constructor(private httpClient:HttpClient) { }
 
   ngOnInit(): void {
+    this.getProducts();
   }
+
+  getProducts(){
+    this.httpClient.get<ProductResponseModel>(this.apiUrl).subscribe((response)=> {
+      this.products=response.data
+    });
+  }
+
+   
 
 }
