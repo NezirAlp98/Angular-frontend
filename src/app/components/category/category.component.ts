6a392c66ee4afdc0d407a7515112f03957f7d60d
nezir alp arslan <nezirarslan1234@gmail.com>
2021-04-04 18:27:39 +0300

    4 nisan commit 2

--- src/app/components/category/category.component.ts
+++ src/app/components/category/category.component.ts
@@ -10,6 +10,7 @@ import { CategoryService } from 'src/app/services/category.service';
 export class CategoryComponent implements OnInit {
 
   categories:Category[]=[];
+  currentCategory:Category;
 
   constructor(private categoryService:CategoryService) { }
 
@@ -22,6 +23,17 @@ export class CategoryComponent implements OnInit {
       this.categories=response.data
     })
 
+  }
+  setCurrentCategory(category:Category){
+    this.currentCategory=category;
+  }
+
+  getCurrentCategoryClass(category:Category){
+    if(category==this.currentCategory){
+      return "list-group-item active"
+    }else{
+      return "list-group-item"
+    }
   }
 
 
