588169fc0b89ef17425d750119ef8cf2710b0455
nezir alp arslan <nezirarslan1234@gmail.com>
2021-04-16 19:12:05 +0300

    16 nisan son commit

--- src/app/components/product-add/product-add.component.ts
+++ src/app/components/product-add/product-add.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {FormGroup,FormBuilder,FormControl,Validators} from "@angular/forms"
+import { ToastrService } from 'ngx-toastr';
+import { ProductService } from 'src/app/services/product.service';
 
 @Component({
   selector: 'app-product-add',
@@ -9,7 +11,7 @@ import {FormGroup,FormBuilder,FormControl,Validators} from "@angular/forms"
 export class ProductAddComponent implements OnInit {
 
   productAddForm:FormGroup;
-  constructor(private formBuilder:FormBuilder) { }
+  constructor(private formBuilder:FormBuilder,private productService:ProductService,private toastrService:ToastrService) { }
 
   ngOnInit(): void {
     this.createProductAddForm();
@@ -25,8 +27,28 @@ export class ProductAddComponent implements OnInit {
 
   }
   add(){
-    let productModel=Object.assign({},this.productAddForm.value)
-    console.log(productModel) 
+    if(this.productAddForm.valid){
+      let productModel=Object.assign({},this.productAddForm.value)
+      this.productService.add(productModel).subscribe(response=>{
+        
+        this.toastrService.success(response.message,"Başarılı")
+      },responseError=>{
+        if(responseError.error.Errors.length>0){
+          for (let i = 0; i < responseError.error.Errors.length; i++) {
+            this.toastrService.error(responseError.error.Errors[i].ErrorMessage,"Doğrulama hatası")
+            
+          }
+          
+        }
+        
+        
+    })
+      
+    }else{
+      this.toastrService.error("Formunuz eksik","Dikkat")
+    }
+    
+    
   }
 
 }
